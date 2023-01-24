b962e5c27b2694fb7fe9819dc1edc57138d7e494
Ban Alex <alexbanut10@gmail.com>
2023-01-24 22:00:32 +0200

    map functionality almos ready, need to change the mongoose schema to numbers for loaction and also label styling for the pin point

--- licenta_fe/src/app/mapquest/mapquest.component.ts
+++ licenta_fe/src/app/mapquest/mapquest.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { LocWrapper } from '../locations/location.model';
 import { LocationsService } from '../services/locations/locations.service';
 import { mapStyles } from './mapStyles';
+
+export interface locationMarker {
+    label: string;
+    lat: number;
+    lng: number;
+}
 
 @Component({
     selector: 'app-mapquest',
     templateUrl: './mapquest.component.html',
     styleUrls: ['./mapquest.component.css']
 })
-export class MapquestComponent implements OnInit {
+export class MapquestComponent implements OnInit, OnDestroy {
     locationsSubscirption: Subscription | undefined;
-    locationsObje?: LocWrapper;
+    locationsObj?: LocWrapper;
+    locationsMarkers: locationMarker[] = [];
     private sibiuGeo = {
         lat: 45.79,
         lng: 24.13
@@ -25,10 +32,38 @@ export class MapquestComponent implements OnInit {
     private map!: google.maps.Map;
 
     ngOnInit(): void {
-        this.loadMap();
+        this.fetchLocations();
     }
 
-    loadMap() {
+    ngOnDestroy(): void {
+        this.locationsSubscirption?.unsubscribe();
+    }
+
+    fetchLocations() {
+        const locationWrapperObs: Observable<LocWrapper> = this.locationsService.getAllLocations();
+
+        this.locationsSubscirption = locationWrapperObs.subscribe({
+            next: (response) => {
+                (this.locationsObj = response), console.log(this.locationsObj), this.mapLocationsInfo(this.locationsObj);
+            },
+            error: (error) => console.log(error)
+        });
+    }
+
+    mapLocationsInfo(locationsObj: LocWrapper) {
+        for (let location of locationsObj.locations) {
+            let oneLocMarker = {
+                label: location.name,
+                lat: parseFloat(location.location.lat),
+                lng: parseFloat(location.location.long)
+            };
+            this.locationsMarkers.push(oneLocMarker);
+        }
+        console.log(this.locationsMarkers);
+        this.loadMap(this.locationsMarkers);
+    }
+
+    loadMap(locationsMarkers: locationMarker[]) {
         let loader = new Loader({
             apiKey: environment.googleAPI_KEY
         });
@@ -47,8 +82,16 @@ export class MapquestComponent implements OnInit {
             //     position: this.sibiuGeo,
             //     map: this.map
             // });
+            for (let locationMarker of locationsMarkers) {
+                const marker = new google.maps.Marker({
+                    label: locationMarker.label,
+                    position: {
+                        lat: locationMarker.lat,
+                        lng: locationMarker.lng
+                    }
+                });
+                console.log(marker);
+            }
         });
     }
-
-    fetchLocations() {}
 }
