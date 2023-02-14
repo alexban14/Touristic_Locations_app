520028db70f60120bb9d983213a54af8661ae802
Alex Ban <alexbanut10@gmail.com>
2023-02-14 17:43:05 +0200

    added to the event show page a google map that points to the location the event will happen and also changed the position of the create event button

--- licenta_fe/src/app/eventsPage/show-page-event/show-page-event.component.ts
+++ licenta_fe/src/app/eventsPage/show-page-event/show-page-event.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Loader } from '@googlemaps/js-api-loader';
 import { Subscription } from 'rxjs';
 import { EventsService } from 'src/app/services/eventsPage/events.service';
+import { environment } from 'src/environments/environment';
 import { Event, OneEventWrapper } from '../event.model';
+import { eventMapStyles } from 'src/app/mapquest/mapStylesEvent';
 
 @Component({
     selector: 'app-show-page-event',
@@ -24,8 +27,43 @@ export class ShowPageEventComponent implements OnInit {
                 next: (response: OneEventWrapper) => {
                     console.log(response);
                     this.eventObj = response.event;
+                    this.loadMap(response.event);
                 },
                 error: (err) => console.log(err)
             });
+    }
+
+    title = 'google-maps';
+    private map!: google.maps.Map;
+
+    loadMap(event: Event) {
+        let loader = new Loader({
+            apiKey: environment.googleAPI_KEY
+        });
+
+        loader.load().then(() => {
+            console.log('Loaded map');
+
+            this.map = new google.maps.Map(document.getElementById('map')!, {
+                center: {
+                    lat: event.location.lat,
+                    lng: event.location.long
+                },
+                zoom: 14,
+                styles: eventMapStyles
+            });
+
+            const img = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+
+            const marker = new google.maps.Marker({
+                title: event.name,
+                position: {
+                    lat: event.location.lat,
+                    lng: event.location.long
+                },
+                icon: img
+            });
+            marker.setMap(this.map);
+        });
     }
 }
